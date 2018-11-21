874c0a59c74d6c922b36d36408c539978ecf5488
zacharysang <zacharysang@gmail.com>
2018-11-21 12:56:14 -0500

    Add querying with mongodb
    * To connect, must set MONGO_HOST, MONGO_PORT, MONGO_USER, and MONGO_PASS env vars
    * Add function to take query list of lists and return urls in db

--- query.py
+++ query.py
@@ -2,6 +2,7 @@ import sys
 import os
 import re
 from pymongo import MongoClient
+from pprint import pprint
 
 PAREN_OPEN = "("
 PAREN_CLOSE = ")"
@@ -9,11 +10,58 @@ QUOTE = "\""
 AND = "&"
 OR = "|"
 
+MONGO_HOST = os.environ["MONGO_HOST"]
+MONGO_PORT = os.environ["MONGO_PORT"]
+MONGO_USER = os.environ["MONGO_USER"]
+MONGO_PASS = os.environ["MONGO_PASS"]
+
+MONGO_NAME = "dki"
+MONGO_COLLECTION = "Keywords"
+
+def query_keyword(keyword, db_collection):
+  # Perform query on the db
+  results = list(db_collection.find({"word": keyword})) 
+  
+  # Extract the urls lists from all returned entries (dictionaries)
+  urls = list(map(lambda entry: entry["urls"], results))
+
+  # Flatten the list
+  urls = [url for sublist in urls for url in sublist]
+  
+  return urls
+
 def remove_bounding_chars(string, ch1, ch2):
   if (string[0] == ch1 and string[len(string)-1] == ch2):
     return string[1:len(string)-1]
   else:
     return string
+
+# Take query (list of lists) and return list of urls
+def get_urls(query, db_collection):
+  # Query and perform unions and intersections as needed
+  urls = None
+  for group in groups:
+    # build list of urls for the current disjoint group
+    group_urls = []
+    for word in group:
+      group_urls = set(group_urls) | set(query_keyword(word, urls_collection))
+  
+    # If urls is defined, set it to take intersection of current group urls with the accumulated urls
+    # Otherwise set it to the current group urls
+    if urls != None:
+      urls = set(urls) & set(group_urls)
+    else:
+      urls = group_urls
+
+  return list(urls)
+
+## End of function definitions, start of code ##
+
+# Setup database connection
+client = MongoClient(MONGO_HOST, int(MONGO_PORT))
+db = client[MONGO_NAME]
+db.authenticate(MONGO_USER, MONGO_PASS)
+urls_collection = db[MONGO_COLLECTION]
 
 # Validate the number of input args
 if len(sys.argv) != 2:
@@ -32,4 +80,6 @@ if (re.match(r"(\([a-zA-Z]+(\|[a-zA-Z]+)+\)|[a-zA-Z]+)(\&(\([a-zA-Z]+(\|[a-zA-Z]
 groupsStrs = query.split(AND)
 groups = list(map(lambda group: remove_bounding_chars(group, PAREN_OPEN, PAREN_CLOSE).split(OR), groupsStrs)) 
 
-print("Processing query: " + str(groups))
+urls = get_urls(groups, urls_collection)
+
+print("Results: " + str(urls))

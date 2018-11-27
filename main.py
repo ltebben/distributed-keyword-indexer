a8d44dce2329301879ae23ba77ffa9df1606bb94
zacharysang <zacharysang@gmail.com>
2018-11-27 13:59:54 -0500

    Words persisted in main.py

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import os
 from mpi4py import MPI
 from scrape import Scrape
+from pymongo import MongoClient
+
 # Define constants for config files
+
 SOURCES_LOC = "sources.txt"
-s = Scrape()
-# Get cluster variables
+MONGO_HOST = os.environ["MONGO_HOST"]
+MONGO_PORT = os.environ["MONGO_PORT"]
+MONGO_USER = os.environ["MONGO_USER"]
+MONGO_PASS = os.environ["MONGO_PASS"]
+
+MONGO_NAME = "dki"
+MONGO_COLLECTION = "Keywords"
+
+# Initialize database connection
+client = MongoClient(MONGO_HOST, int(MONGO_PORT))
+db = client[MONGO_NAME]
+db.authenticate(MONGO_USER, MONGO_PASS)
+urls_collection = db[MONGO_COLLECTION]
+
+# Initialize scraper
+s = Scrape(collection=urls_collection)
+
+# Initialize mpi cluster variables
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
@@ -24,7 +44,7 @@ if rank == 0:
     link = sources.pop(0)
     comm.send(link, dest=ind)
     newlinks = comm.recv(source=ind)
-    print(newlinks)
+    
     for link in newlinks:
       if link not in explored:
         sources.append(link)
@@ -33,9 +53,17 @@ if rank == 0:
     #   w.write("\n")
     #   w.write('\n'.join(newlinks))
     i += 1
-else:
+else: 
   # Wait to receive a source from the master
   source = comm.recv(source=0)
+
   s.setUrl(source.strip())
   keywords, links = s.scrape()
-  comm.send(links, dest=0)
+
+  print("number of links: " + str(len(links)))
+
+  # Persist keywords to the database
+  s.submitWords(keywords)
+
+  # Send new links back to the master queue
+  comm.send(links, dest=0)  

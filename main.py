098e1f98077a493639769433820bc8988035d92c
Kurt Lewis <kurt.jlewis15@gmail.com>
2018-12-01 10:10:32 -0500

    Asynchronous message passing!

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import copy
 from mpi4py import MPI
 from scrape import Scrape
 from pymongo import MongoClient
@@ -39,31 +40,41 @@ if rank == 0:
   i = 1
   
   # TODO: remove i<10 and put this stuff in the db instead of a local array
-  while sources and i<10:
-    ind = i%size
-    link = sources.pop(0)
-    comm.send(link, dest=ind)
-    newlinks = comm.recv(source=ind)
-    
-    for link in newlinks:
-      if link not in explored:
-        sources.append(link)
-        explored.add(link)
-    # with open(SOURCES_LOC, 'a') as w:
-    #   w.write("\n")
-    #   w.write('\n'.join(newlinks))
-    i += 1
+
+  # Create lists of messages/lists
+  # The given index in these lists corresponds to the worker of id index + 1
+  receiveMessages = list()
+  receiveLinks = list()
+  sendMessages = list()
+  for idx in range(size - 1):
+    receiveLinks.append(None)
+    receiveMessages.append(comm.Irecv(receiveLinks[idx], source=idx+1))
+  # do stuff
+  while source and i < 10:
+    for idx, req in enumerate(receiveMessages):
+      # Check to see if the request has come back yet
+      if req.test():
+        links = copy.copy(receiveLinks[idx])
+        sendMessages.append(comm.Isend(sources.pop(0), dest=idx+1))
+        receiveLinks[idx] = None
+        receiveMessages[idx] = comm.Irecv(receiveLinks[idx], source=idx+1);
+        for link in links:
+          if link not in explored:
+            sources.append(link)
+            explored.add(link)
+        i+=1
 else: 
-  # Wait to receive a source from the master
-  source = comm.recv(source=0)
+  while True:
+    # Wait to receive a source from the master
+    source = comm.recv(source=0)
 
-  s.setUrl(source.strip())
-  keywords, links = s.scrape()
+    s.setUrl(source.strip())
+    keywords, links = s.scrape()
 
-  print("number of links: " + str(len(links)))
+    print("number of links: " + str(len(links)))
 
-  # Persist keywords to the database
-  s.submitWords(keywords)
+    # Persist keywords to the database
+    s.submitWords(keywords)
 
-  # Send new links back to the master queue
-  comm.send(links, dest=0)  
+    # Send new links back to the master queue
+    comm.send(links, dest=0)  
